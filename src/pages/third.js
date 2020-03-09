38cc1760c9575edda5f3882a918408e4eaa66831
Taynara Pecorario <taynara_rp22@hotmail.com>
2020-03-09 11:52:40 -0300

    cropper:implementação do upload de foto

--- src/pages/third.js
+++ src/pages/third.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 import NextPage from '../components/btn-page';
-import Editor from '../components/Editor';
+import Cropper from "cropperjs";
 
-//Aprendendo a usar o Cropper https://github.com/fengyuanchen/cropperjs
+//Aprendendo a usar o Cropper https://github.com/fengyuanchen/cropperjs, modal e
+//canvas
+
+// function Example() {
+
+  
+//   return();
+// }
 
 class ThirdPractice extends Component {
 
+  state = {
+    image: {}
+  };
+
+  onChangeInput = event => {
+    const file = event.target.files;
+    console.log("FILE", file);
+
+    var reader = new FileReader();
+
+    reader.onload = function () {
+      var dataURL = reader.result;
+      var output = document.getElementById('foto');
+      output.src = dataURL;
+    };
+    reader.readAsDataURL(file[0]);
+  }
+
   render() {
+
     return (
       <div className="Cropper">
-        <Editor />
+        <div className="input-foto">
+          <input
+            type="file"
+            onChange={obj => this.onChangeInput(obj)}
+            name="foto"
+          />
+        </div>
+
+        <div className="img-profile">
+          <img id="foto" />
+        </div>
       </div>
-    );
+    )
   }
 }
 
